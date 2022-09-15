2ea4b36210cb98e1070792ec882cc4d17724aadb
xeniaBch <kseniabekhtgold@gmail.com>
2022-09-15 19:55:57 +0200

    HW08_09 array task

--- src/homework/array/Array.java
+++ src/homework/array/Array.java
@@ -7,11 +7,13 @@ public class Array {
    public static  boolean isMultiply(int[]arr, int value) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
-                if (i != j && arr[i] * arr[j] == value) {
+                if (i < j && arr[i] * arr[j] == value) {
                     return true;
                 }
             }
         }
         return false;
     }
+
+
 }
